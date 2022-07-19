3b53e3164a0800a2565934601e17ae8e14d397ff
Renam <renamgustavo@live.com>
2022-07-19 15:36:25 -0300

    fializado curso 6  modulo 3

--- Curso-Java-parte-2-Introdu-o-Orienta-o-a-Objetos-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencia.java
+++ Curso-Java-parte-2-Introdu-o-Orienta-o-a-Objetos-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteArrayReferencia.java
@@ -1,16 +1,29 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencia {
     public static void main(String[] args) {
-        ContaCorrente[] contas = new ContaCorrente[5];
+        Object[] referencias = new Object[5];
 
         ContaCorrente cc1 = new ContaCorrente(111,1111);
         ContaCorrente cc2 = new ContaCorrente(222,2222);
-        contas[0] = cc1;
-        contas[1] = cc2;
+        referencias[0] = cc1;
+        referencias[1] = cc2;
+
+        Cliente cliente = new Cliente();
+
+        referencias[2] = cliente;
+
+//        Object referenciaGenerica = referencias[1];
+//
+//        System.out.println(referencias[1].getNumero());
+//
+//        ContaPoupanca ref = (ContaPoupanca) referencias[1];
+
 
         System.out.println(cc1.getAgencia());
         System.out.println(cc2.getAgencia());
