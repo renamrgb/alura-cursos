fe2eb7c100bea2ed9bee6e32d2b59ca7934f9ec1
Renam <renamgustavo@live.com>
2022-07-20 09:41:55 -0300

    Curso 6 modulo 7

--- Curso-Java-parte-2-Introdu-o-Orienta-o-a-Objetos-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ Curso-Java-parte-2-Introdu-o-Orienta-o-a-Objetos-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -8,7 +8,7 @@ package br.com.bytebank.banco.modelo;
  *
  * @version 0.1
  */
-public abstract class Conta {
+public abstract class Conta implements Comparable<Conta>{
 
     protected double saldo;
     private int agencia;
@@ -93,6 +93,10 @@ public abstract class Conta {
     }
 
 
+    @Override
+    public int compareTo(Conta c) {
+        return Double.compare(this.saldo, c.saldo);
+    }
 
     @Override
     public String toString() {
---
+++ Curso-Java-parte-2-Introdu-o-Orienta-o-a-Objetos-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteWrappers.java
@@ -0,0 +1,29 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteWrappers {
+    public static void main(String[] args) {
+
+
+        int idade = 29;
+
+        Integer idadeRef = Integer.valueOf(29);
+
+        int valor = idadeRef.intValue();
+
+        String s = "12";
+
+
+        System.out.println(idadeRef.doubleValue());
+
+        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.valueOf(s));
+
+        System.out.println(Integer.parseInt(s));
+        List<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(idadeRef);
+
+    }
+}
