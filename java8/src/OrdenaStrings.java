e52abc4765df062ca4a359dfb79bd6f4fc7f56f3
renamrgb <renamgustavo@live.com>
2022-07-27 17:48:07 -0300

    Curso 9 aula 3

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
@@ -12,9 +13,12 @@ public class OrdenaStrings {
 
         // Collections.sort(palavras, comparator);
 
-        palavras.sort((s1, s2) ->
-            Integer.compare(s1.length(), s2.length())
-        );
+//        palavras.sort((s1, s2) ->
+//            Integer.compare(s1.length(), s2.length())
+//        );
+//
+        palavras.sort(Comparator.comparing(s -> s.length()));
+        palavras.sort(Comparator.comparing(String::length));
 
         System.out.println(palavras);
 //
