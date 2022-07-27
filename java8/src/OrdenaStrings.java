3901ddf214ddc33b736eca3d59e12f6e02c743c8
renamrgb <renamgustavo@live.com>
2022-07-27 17:35:17 -0300

    Curso 9 aula 2

--- java8/src/OrdenaStrings.java
+++ java8/src/OrdenaStrings.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
@@ -11,34 +9,22 @@ public class OrdenaStrings {
         palavras.add("editora casa do codigo");
         palavras.add("caelum");
 
-        Comparator<String> comparator = new ComparadorPorTamanho();
-       // Collections.sort(palavras, comparator);
 
-        palavras.sort(comparator);
+        // Collections.sort(palavras, comparator);
+
+        palavras.sort((s1, s2) ->
+            Integer.compare(s1.length(), s2.length())
+        );
+
         System.out.println(palavras);
 //
 //        for (String p: palavras){
 //            System.out.println(p);
 //        }
 
-        palavras.forEach(p ->{
-            System.out.println(p);
-        });
+        palavras.forEach(p ->
+                System.out.println(p)
+        );
 
-
-    }
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-//        if (s1.length() < s2.length())
-//            return -1;
-//        if (s1.length() > s2.length())
-//            return 1;
-//        return 0;
-
-        return s1.length() - s2.length();
     }
 }
