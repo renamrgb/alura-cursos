e4ead9f00fbab2c7109ee1250f9ac301ae4bcbda
renamrgb <renamgustavo@live.com>
2022-07-28 16:35:45 -0300

    Curso 10 aula 4

---
+++ 2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -0,0 +1,13 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+import java.math.BigDecimal;
+
+public class ReajusteService {
+    public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
+            BigDecimal percentual = funcionario.getSalario().multiply(desempenho.percentualReajuste());
+            funcionario.reajustarSalario(percentual);
+    }
+}
